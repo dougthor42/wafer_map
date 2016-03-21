0e9f816827599a82d967442d2bafa7145278c93e
Douglas Thor <dthor@transphormusa.com>
2016-03-21 14:20:55 -0700

    Added __main__.py

---
+++ wafer_map/__main__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+<insert 1-line description here>
+
+Created on Mon Mar 21 14:08:06 2016
+
+@author: dthor
+
+Usage:
+    __main__.py
+
+Options:
+    -h --help           # Show this screen.
+    --version           # Show version.
+
+"""
+
+import example
+
+example.main()
