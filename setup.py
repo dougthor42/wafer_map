dougthor42/wafer_map

commit e63b724ea85b16e334dcc002511e330e43c2bf8a

Update setup.py's `requires` directive with versions.

setup.py
@@ -56,5 +56,5 @@ setup(
     packages=find_packages(),
     classifiers=classifiers,
 
-    requires=["wxPython_Phoenix"],
+    requires=["wxPython>=4.0.0", "numpy>=1.12.1", "colour>=0.1.4"],
     )

commit de8b729afbad43de95aadfbf33f89f08f87e2315

Fix error in setup.py introduced in e63b724ea85b16e334dcc002511e330e43c2bf8a

setup.py
@@ -56,5 +56,5 @@ setup(
     packages=find_packages(),
     classifiers=classifiers,
 
-    requires=["wxPython>=4.0.0", "numpy>=1.12.1", "colour>=0.1.4"],
+    install_requires=["wxPython>=4.0.0", "numpy>=1.12.1", "colour>=0.1.4"],
     )