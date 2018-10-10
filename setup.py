e63b724ea85b16e334dcc002511e330e43c2bf8a
Douglas Thor <dthor@transphormusa.com>
2018-10-10 09:48:37 -0700

    Update setup.py's `requires` directive with versions.

--- setup.py
+++ setup.py
@@ -56,5 +56,5 @@ setup(
     packages=find_packages(),
     classifiers=classifiers,
 
-    requires=["wxPython_Phoenix"],
+    requires=["wxPython>=4.0.0", "numpy>=1.12.1", "colour>=0.1.4"],
     )
